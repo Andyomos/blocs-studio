458b1f52c88f8b0ad6c8fcd783b72ad9532e72e6
Anderson Omoragbon <61177055+Andyomos@users.noreply.github.com>
2022-02-18 00:48:14 -0700

    Added icon to site browser and added titles to homeroute

--- servers/controllers/homeRoutes.js
+++ servers/controllers/homeRoutes.js
@@ -1,7 +1,7 @@
 export const homePage = (req, res) => {
-  res.status(200).render("index");
+  res.status(200).render("index", {title: "Home"});
 };
 
 export const portfolioPage = (req, res) => {
-  res.status(200).render("portfolio");
+  res.status(200).render("portfolio", {title: "Portfolio"});
 };
