ca51a552a24661d15aba1c7122991590337196db
Anderson Omoragbon <61177055+Andyomos@users.noreply.github.com>
2022-02-18 00:36:02 -0700

    Added new files and modified ejs files, build canvas showcase

--- app.js
+++ app.js
@@ -17,5 +17,4 @@ app.use((req, res, next) => {
     res.status(404).render("404");
 });
 
-
 export default app;
--- servers/server.js
+++ servers/server.js
@@ -9,8 +9,12 @@ import app from "../app.js";
 const PORT = process.env.PORT || 8080;
 const DB_URL_CONNECTOR = process.env.DB_URL_CONNECTOR;
 
-mongoose.connect(DB_URL_CONNECTOR);
-
-http.createServer(app).listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}`);
-});
+mongoose.connect(DB_URL_CONNECTOR)
+  .then((result) => {
+    http.createServer(app).listen(PORT, () => {
+      console.log(`Listening on port: ${PORT}`);
+      console.log(`Database connection established: port ${PORT} is connected`);
+    });
+    
+  })
+  .catch((err) => { console.log(`Error connecting to database: ${err.message}`); });
